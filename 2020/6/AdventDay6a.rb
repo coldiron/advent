d8821a8a929140bb204747424b8a38df8af18b62
Richard Mitsuk Lavitt <r.mitsuklavitt@gmail.com>
2020-12-09 12:22:39 -0600

    day 6 done

---
+++ 2020/6/AdventDay6a.rb
@@ -0,0 +1,44 @@
+class CustomsGroup
+  attr_reader :answers
+
+  def initialize(answers)
+    @answers = answers
+  end
+
+  def unique_answers
+    @answers.strip.gsub(/\n/, '').split('').uniq.length
+  end
+end
+
+class CustomsGroups
+  def initialize
+    @groups = []
+  end
+
+  def from_file(filename)
+    get_groups(filename)
+  end
+
+  def sum_unique_answers
+    @groups.map do |group|
+      group.unique_answers
+    end.sum
+  end
+
+  private
+
+  def get_groups(filename)
+    file = File.open(filename)
+    groups = file.read.split("\n\n")
+    file.close
+    groups.each do |group|
+      @groups << CustomsGroup.new(group.strip)
+    end
+  end
+end
+
+groups = CustomsGroups.new
+
+groups.from_file('input')
+
+puts groups.sum_unique_answers
---
+++ 2020/6/AdventDay6b.rb
@@ -0,0 +1,58 @@
+class CustomsGroup
+  attr_reader :answers
+
+  def initialize(answers)
+    @members = answers.split("\n")
+  end
+
+  def shared_answers_count
+    answered_yes = Hash.new
+
+    @members.each do |member|
+      member.split('').each do |answer|
+        if answered_yes.key?(answer)
+          answered_yes[answer] += 1
+        else
+          answered_yes[answer] = 1
+        end
+      end
+    end
+
+    answered_yes.keep_if { |_,  value| value == @members.length }
+
+    answered_yes.length
+  end
+end
+
+class CustomsGroups
+  def initialize
+    @groups = []
+  end
+
+  def from_file(filename)
+    get_groups(filename)
+  end
+
+  def sum_shared_answers
+    @groups.map do |group|
+      group.shared_answers_count
+    end.sum
+  end
+
+  private
+
+  def get_groups(filename)
+    file = File.open(filename)
+    groups = file.read.split("\n\n")
+    file.close
+    groups.each do |group|
+      @groups << CustomsGroup.new(group)
+    end
+  end
+end
+
+groups = CustomsGroups.new
+
+groups.from_file('input')
+
+puts groups.sum_shared_answers
