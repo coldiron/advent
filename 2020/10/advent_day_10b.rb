4e14a1e2bdfbfee1fd0621641ff65d6eddcf2859
Richard Mitsuk Lavitt <r.mitsuklavitt@gmail.com>
2020-12-10 14:23:44 -0600

    refactor day 10

--- 2020/10/advent_day_10b.rb
+++ 2020/10/advent_day_10b.rb
@@ -2,6 +2,7 @@ class Chargers
   attr_reader :chargers, :differences
 
   def initialize
+    @memo = {}
     @chargers = []
     @differences = []
   end
@@ -15,47 +16,31 @@ class Chargers
     @chargers = @chargers.map { |charger| charger.to_i } # for some reason I get an array of strings even if I set values with to_i
     @chargers.append(@chargers.max + 3) # add device's built-in charger
     @chargers.append(0) # add airplane's outlet
+    @chargers.sort!
   end
 
-  def permutations_memo(joltage)
-    perms = {}
-    permutations(joltage, perms)
-  end
-
-  def permutations(joltage, memo)
-    memo[joltage] = 0
+  def permutations(joltage)
+    @memo[joltage] = 0
     if joltage == @chargers.max
-      memo[joltage] = 1
+      @memo[joltage] = 1
       return 1
     end
 
     paths = 0
-    if has_charger?(joltage + 1)
-      if memo[joltage + 1].nil?
-        paths += permutations(joltage + 1, memo)
-        memo[joltage + 1] = paths
-      else
-        paths += memo[joltage + 1]
-      end
-    end
-    if has_charger?(joltage + 2)
-      if memo[joltage + 2].nil?
-        paths += permutations(joltage + 2, memo)
-        memo[joltage + 2] = paths
-      else
-        paths += memo[joltage + 2]
-      end
-    end
-    if has_charger?(joltage + 3)
-      if memo[joltage + 3].nil?
-        paths += permutations(joltage + 3, memo)
-        memo[joltage + 3] = paths
-      else
-        paths += memo[joltage + 3]
-      end
-    end
+    paths = paths_for_joltage(joltage + 1, paths)
+    paths = paths_for_joltage(joltage + 2, paths)
+    paths = paths_for_joltage(joltage + 3, paths)
     paths
   end
+
+  def add_joltage_differences
+    @chargers.length.times do |i|
+      @differences.append @chargers[i + 1] - @chargers[i] unless @chargers[i + 1].nil?
+    end
+    @differences.sort!
+  end
+
+  private
 
   def has_charger?(joltage)
     @chargers.include? joltage
@@ -65,15 +50,16 @@ class Chargers
     @chargers.append(joltage.to_i)
   end
 
-  def sort_by_joltage
-    @chargers.sort!
-  end
-
-  def add_joltage_differences
-    @chargers.length.times do |i|
-      @differences.append @chargers[i + 1] - @chargers[i] unless @chargers[i + 1].nil?
+  def paths_for_joltage(joltage, paths)
+    if has_charger?(joltage)
+      if @memo[joltage].nil?
+        paths += permutations(joltage)
+        @memo[joltage] = paths
+      else
+        paths += @memo[joltage]
+      end
     end
-    @differences.sort!
+    paths
   end
 end
 
@@ -81,6 +67,4 @@ chargers = Chargers.new
 
 chargers.from_file('input')
 
-chargers.sort_by_joltage
-
-puts chargers.permutations_memo(0).to_s
+puts chargers.permutations(0).to_s
