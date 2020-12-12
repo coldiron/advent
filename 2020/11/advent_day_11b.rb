282876e71ef5eee83dda4418c51074440e81a663
Richard Mitsuk Lavitt <r.mitsuklavitt@gmail.com>
2020-12-12 15:00:13 -0600

    refactor day 11 part 2

--- 2020/11/advent_day_11b.rb
+++ 2020/11/advent_day_11b.rb
@@ -49,160 +49,51 @@ class Rows
     @seats_length = 0
   end
 
-  def sitting_round
+  def musical_chairs
     new_rows = Marshal.load(Marshal.dump(@rows)) # Using marshal to 'deep clone'
 
     @rows.each_with_index do |row, row_index|
       row.seats.each_with_index do |seat, seat_index|
-        adjacent = 0
-        unless seat.floor?
-          adjacent = check_adjacent(row_index, seat_index)
-          if seat.occupied? && adjacent > 3
-              new_rows[row_index].seats[seat_index].stand!
-          end
-          if seat.empty? && adjacent == 0
-              new_rows[row_index].seats[seat_index].sit!
-          end
-        end
+        next if seat.floor?
+
+        visible = visible_count(row_index, seat_index)
+        new_rows[row_index].seats[seat_index].stand! if seat.occupied? && visible >= 5
+        new_rows[row_index].seats[seat_index].sit! if seat.empty? && visible == 0
       end
     end
     @last_rows = Marshal.load(Marshal.dump(@rows))
     @rows = Marshal.load(Marshal.dump(new_rows))
   end
 
-  def sitting_round_part_two
-    new_rows = Marshal.load(Marshal.dump(@rows)) # Using marshal to 'deep clone'
-
-    @rows.each_with_index do |row, row_index|
-      row.seats.each_with_index do |seat, seat_index|
-        unless seat.floor?
-          visible = check_visible(row_index, seat_index)
-          if seat.occupied? && visible >= 5
-              new_rows[row_index].seats[seat_index].stand!
-          end
-          if seat.empty? && visible == 0
-              new_rows[row_index].seats[seat_index].sit!
-          end
-        end
-      end
-    end
-    @last_rows = Marshal.load(Marshal.dump(@rows))
-    @rows = Marshal.load(Marshal.dump(new_rows))
-  end
-
-  # This is an absolute monstrosity that arose from debugging and i'm just not
-  # going to bother changing it
-  def check_visible(row_index, seat_index)
-    visible = {} #used this to get into a bug, too lazy to change back
-    checking_seat_index = seat_index.dup
-    # Up
-    ((row_index - 1).downto(0)).each do |i|
-      if @rows[i].seats[checking_seat_index].occupied?
-        visible[:up] = true
-      end
-      unless @rows[i].seats[checking_seat_index].floor?
-        break
-      end
-    end
-    # Up/Right
-    checking_seat_index = seat_index.dup
-    ((row_index - 1).downto(0)).each do |i|
-      checking_seat_index += 1
-      unless outside_index?(i, checking_seat_index)
-        if @rows[i].seats[checking_seat_index].occupied?
-          visible[:up_right] = true
-        end
-        unless @rows[i].seats[checking_seat_index].floor?
-          break
-        end
-      end
-    end
-    checking_seat_index = seat_index.dup
-    # Right
-    (seat_index + 1..@seats_length - 1).each do |j|
-      if @rows[row_index].seats[j].occupied?
-        visible[:right] = true
-      end
-      unless @rows[row_index].seats[j].floor?
-        break
-      end
-    end
-    checking_seat_index = seat_index.dup
-    # Right/Down
-    (row_index + 1..@seats_length - 1).each do |i|
-      checking_seat_index += 1
-      unless outside_index?(i, checking_seat_index)
-        if @rows[i].seats[checking_seat_index].occupied?
-          visible[:right_down] = true
-        end
-        unless @rows[i].seats[checking_seat_index].floor?
-          break
-        end
-      end
-    end
-    checking_seat_index = seat_index.dup
-    # Down
-    (row_index + 1..@rows.length - 1).each do |i|
-      if @rows[i].seats[checking_seat_index].occupied?
-        visible[:down] = true
-      end
-      unless @rows[i].seats[checking_seat_index].floor?
-        break
-      end
-    end
-    checking_seat_index = seat_index.dup
-    # Down/Left
-    (row_index + 1..@seats_length - 1).each do |i|
-      checking_seat_index -= 1
-      unless outside_index?(i, checking_seat_index)
-        if @rows[i].seats[checking_seat_index].occupied?
-          visible[:down_left] = true
-        end
-        unless @rows[i].seats[checking_seat_index].floor?
-          break
-        end
-      end
-    end
-    checking_seat_index = seat_index.dup
-    # Left
-    ((seat_index - 1).downto(0)).each do |j|
-      unless outside_index?(row_index, j)
-        if @rows[row_index].seats[j].occupied?
-          visible[:left] = true
-        end
-        unless @rows[row_index].seats[j].floor?
-          break
-        end
-      end
-    end
-    checking_seat_index = seat_index.dup
-    # Left/Up
-    ((row_index - 1).downto(0)).each do |i|
-      checking_seat_index -= 1
-      unless outside_index?(i, checking_seat_index)
-        if @rows[i].seats[checking_seat_index].occupied?
-          visible[:leftup] = true
-        end
-        unless @rows[i].seats[checking_seat_index].floor?
-          break
-        end
-      end
-    end
+  def visible_count(row_index, seat_index)
+    directions = [[-1, -1], [-1, 0], [-1, 1],
+                  [ 0, -1],          [ 0, 1], # rubocop:disable all
+                  [ 1, -1], [ 1, 0], [ 1, 1]] # rubocop:disable all
     count = 0
-    #puts visible.inspect
-    visible.each_value do |v|
-      count +=1
+    directions.each do |direction| 
+      count += 1 if visible_in_direction(row_index, seat_index, direction)
     end
     count
+  end
+
+  def visible_in_direction(row_index, seat_index, direction)
+    visible = false
+    until visible
+      row_index += direction[0]
+      seat_index += direction[1]
+      break if outside_index?(row_index, seat_index)
+      square = @rows[row_index].seats[seat_index]
+      visible = true if square.occupied?
+      break unless square.floor?
+    end
+    visible
   end
 
   def filled_seats
     count = 0
     @rows.each do |row|
       row.seats.each do |seat|
-        if seat.occupied?
-          count += 1
-        end
+        count += 1 if seat.occupied?
       end
     end
     count
@@ -219,24 +110,8 @@ class Rows
     output
   end
 
-
-  def check_adjacent(row_index, seat_index)
-    adjacent = 0
-
-    (row_index - 1..row_index + 1).each do |i|
-      (seat_index - 1..seat_index + 1).each do |j|
-        unless outside_index?(i, j)
-          if @rows[i].seats[j].occupied?
-            adjacent += 1 unless(i == row_index && j == seat_index)
-          end
-        end
-      end
-    end
-    adjacent
-  end
-
-  def outside_index?(i, j)
-    i < 0 || i >= @rows.length || j < 0 || j >= @seats_length
+  def to_str
+    to_s
   end
 
   def from_file(filename)
@@ -249,20 +124,28 @@ class Rows
       @rows.append(new_row)
     end
   end
+
+  def resolve_seating
+    rounds = 0
+    last_count = -1
+
+    until filled_seats == last_count
+      last_count = filled_seats
+      musical_chairs
+      rounds += 1
+    end
+  end
+
+  private
+  def outside_index?(i, j)
+    i < 0 || i >= @rows.length || j < 0 || j >= @seats_length
+  end
 end
 
 rows = Rows.new
 
 rows.from_file('input')
 
-rounds = 0
-finished = false
-last_count = -1
-until rows.filled_seats == last_count
-  last_count = rows.filled_seats
-  rows.sitting_round_part_two
-  rounds += 1
-end
-puts rows.filled_seats
-puts rounds
+rows.resolve_seating
 
+puts "Final number of seats filled: #{rows.filled_seats}."
