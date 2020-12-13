763a4324b9f8e715ce38e8b804241b67cc9f1336
Richard Mitsuk Lavitt <r.mitsuklavitt@gmail.com>
2020-12-13 17:42:30 -0600

    added recursive version to day 13b

--- 2020/13/b.rb
+++ 2020/13/b.rb
@@ -13,18 +13,34 @@ class BusSchedule
     time
   end
 
+  def calculate_recursive(time = 0, interval = 1, index = 0)
+    return time if @buses[index].nil?
+    bus, offset = @buses[index]
+    time = calculate_recursive(time + interval, interval, index) unless (time + offset) % bus == 0
+    time = calculate_recursive(time, interval * bus, index + 1)
+  end
+
   def load_from_file(filename)
     offset = 0
     File.readlines(filename, chomp: true)[1].split(',').each do |entry|
-      unless entry == 'x'
-        @buses.append([entry.to_i, offset])
-      end
+      @buses.append([entry.to_i, offset]) unless entry == 'x'
       offset += 1
     end
   end
 end
 
+require 'benchmark' 
+
 buses = BusSchedule.new
 
 buses.load_from_file('input')
+
+non_rec_time = Benchmark.measure { 10000.times { buses.calculate } }
+
+rec_time = Benchmark.measure { 10000.times { buses.calculate_recursive } }
+
+
+puts "Reg: #{non_rec_time}Rec: #{rec_time}"
+
+puts buses.calculate_recursive
 puts buses.calculate
